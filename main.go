bd3a5962a27ebb2db742678564e861c2959540b4
AshwiniGavali <73137617+AshwiniGavali@users.noreply.github.com>
2020-10-28 21:32:19 +0100

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<h1>Welcome to the world of CI/CD with Jenkins CI</h1>")
+		fmt.Fprintf(w, "<h1>Welcome to the world of CI/CD with Jenkins CI & other tools</h1>")
 	})
 
 	http.ListenAndServe(":9090", r)
